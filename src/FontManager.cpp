f767dbbfaf5af9fb17ae6e6970a8bec41c68040b
YevhenHoina <yevhenhoina@gmail.com>
2025-07-03 18:06:47 +0300

    basic font support

--- src/FontManager.cpp
+++ src/FontManager.cpp
@@ -6,5 +6,5 @@ ImFont* FontManager::icon_font = nullptr;
 void FontManager::LoadFonts() {
     ImGuiIO& io = ImGui::GetIO();
     inter_font = io.Fonts->AddFontFromFileTTF("fonts\\static\\Inter_18pt-Regular.ttf", 18.0f);
-    icon_font = io.Fonts->AddFontFromFileTTF("fonts\\icons\\icon-works-webfont.ttf", 24.0f);
+    icon_font = io.Fonts->AddFontFromFileTTF("fonts\\icons\\icon-works-webfont.ttf", 32.0f);
 }
